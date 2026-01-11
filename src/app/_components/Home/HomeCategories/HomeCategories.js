415bdf3d7e5a79ef940bd2f4e5712cf957056dc6
saadable <saadsajid098@gmail.com>
2026-01-11 17:58:28 +0500

    *

--- src/app/_components/Home/HomeCategories/HomeCategories.js
+++ src/app/_components/Home/HomeCategories/HomeCategories.js
@@ -6,10 +6,10 @@ import Image from 'next/image'
 const HomeCategories = () => {
   return (
     <div>
-      <h1 className='text-center text-[28px] my-5 md:mt-0 md:text-[35px] font-bold text-[#2785ca]'>Our Hottest Categories</h1>
+      <h1 className='text-center text-[28px] my-5 md:mt-5 md:text-[35px] font-bold text-[#2785ca]'>Our Hottest Categories</h1>
       <div className="images flex flex-col md:flex md:flex-row items-center justify-center px-5 md:px-0">
-        <Image src={Airpods} alt='Airpods Image' width={1000} height={1000} className='w-[400px]  m-5 rounded-[40px]'/>
-        <Image src={Watches} alt='Watches Image' width={1000} height={1000} className='w-[400px]  m-5 rounded-[40px]'/>
+        <Image src={Airpods} alt='Airpods Image' width={1000} height={1000} className='w-full md:w-[600px] md:h-[400px] m-5 rounded-[40px]'/>
+        <Image src={Watches} alt='Watches Image' width={1000} height={1000} className='w-full md:w-[600px] md:h-[400px] m-5 rounded-[40px]'/>
       </div>
     </div>
   )

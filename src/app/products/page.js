bcae600ca8f55a7941319bd39ffb962f0f9856c1
saadable <saadsajid098@gmail.com>
2026-02-26 21:38:06 +0500

    *

--- src/app/products/page.js
+++ src/app/products/page.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import ProductsList from '@/Components/ProductsList/ProductsList'
 
 const ProductsPage = ({ searchParams }) => {
   const initialQuery = (searchParams && searchParams.query) || ''
-  return <ProductsList initialQuery={initialQuery} />
+  return (
+    <Suspense fallback={<div className='py-12 text-center'>Loading products…</div>}>
+      <ProductsList initialQuery={initialQuery} />
+    </Suspense>
+  )
 }
 
 export default ProductsPage
